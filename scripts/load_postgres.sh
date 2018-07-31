45d38d90aee53f2c5e255f7607eff238a91824f6
michaelsexton <michael.sexton@ga.gov.au>
2018-07-31 11:50:18 +1000

    Check passwords

--- scripts/load_postgres.sh
+++ scripts/load_postgres.sh
@@ -4,9 +4,12 @@ pushd ~ubuntu
 
 unzip src/main/resources/geonetwork-db.zip
 export GEONETWORK_PASSWORD=`date | sha512sum | base64 | head -c 32` &> /dev/null
+echo $GEONETWORK_PASSWORD
 sed -i 's/${password}/'"${GEONETWORK_PASSWORD}"'/' src/main/resources/geonetwork-db.sql
 sudo psql -U postgres -f src/main/resources/geonetwork-db.sql
 
+
+echo $GEONETWORK_PASSWORD
 sed -i 's/${password}/'"${GEONETWORK_PASSWORD}"'/' src/main/webapp/WEB-INF/config-db/jdbc.properties
 
 mvn clean package
