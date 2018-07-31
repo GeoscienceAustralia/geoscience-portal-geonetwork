9b614b7047e69ce66c6ac3db22beb624e042fe9c
michaelsexton <michael.sexton@ga.gov.au>
2018-07-31 10:47:30 +1000

    Work in correct directory

--- scripts/load_postgres.sh
+++ scripts/load_postgres.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+pushd ..
 unzip src/main/resources/geonetwork-db.zip
 export GEONETWORK_PASSWORD=`date | sha512sum | base64 | head -c 32` &> /dev/null
 sed -i 's/${password}/'"${GEONETWORK_PASSWORD}"'/' src/main/resources/geonetwork-db.sql
@@ -8,3 +8,4 @@ sudo psql -U postgres -f src/main/resources/geonetwork-db.sql
 sed -i 's/${password}/'"${GEONETWORK_PASSWORD}"'/' src/main/webapp/WEB-INF/config-db/jdbc.properties
 
 mvn clean deploy
+
