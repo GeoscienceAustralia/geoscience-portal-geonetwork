e252292bbe2979ea9f5b622b6a238ca08cf34593
michaelsexton <michael.sexton@ga.gov.au>
2019-06-07 16:24:01 +1000

    Use SSM to get parameters

--- scripts/load_postgres.sh
+++ scripts/load_postgres.sh
@@ -2,4 +2,11 @@
 
 pushd ~ubuntu/geonetwork
 
+
+DB_PASSWORD=$( aws ssm get-parameters --name ausgin.catalog.db-password --region ap-southeast-2 --with-decryption --query Parameters[0].Value )
+
+DB_PASSWORD=`echo $password | sed -e 's/^"//' -e 's/"$//'`
+
+sed -i 's/__PASSWORD__/'"${DB_PASSWORD}"'/' src/main/webapp/WEB-INF/config-db/jdbc.properties
+
 mvn clean package
