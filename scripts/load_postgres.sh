6ab27e52db4255a894cbdaa2f46dfac7c94e8fa3
michaelsexton <michael.sexton@ga.gov.au>
2019-06-07 17:07:01 +1000

    Configure GeoNetwork installation

--- scripts/load_postgres.sh
+++ scripts/load_postgres.sh
@@ -2,11 +2,13 @@
 
 pushd ~ubuntu/geonetwork
 
-
+# Get RDS password from key store and rewrite JDBC config
 DB_PASSWORD=$( aws ssm get-parameters --name ausgin.catalog.db-password --region ap-southeast-2 --with-decryption --query Parameters[0].Value )
-
 DB_PASSWORD=`echo $DB_PASSWORD | sed -e 's/^"//' -e 's/"$//'`
-
 sed -i 's/__PASSWORD__/'"${DB_PASSWORD}"'/' src/main/webapp/WEB-INF/config-db/jdbc.properties
 
+
+# Install GeoNetwork WAR
 mvn clean package
+rm -rf /var/lib/tomcat8/webapps/ROOT*
+mv target/geonetwork.war /var/lib/tomcat8/webapps/
