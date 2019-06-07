58f7a2caa8a6c6b222b6b3a9bc0287832294c0f6
michaelsexton <michael.sexton@ga.gov.au>
2019-06-07 18:27:13 +1000

    Don't rewrite password

--- scripts/load_postgres.sh
+++ scripts/load_postgres.sh
@@ -4,7 +4,7 @@ pushd ~ubuntu/geonetwork
 
 # Get RDS password from key store and rewrite JDBC config
 DB_PASSWORD=$( aws ssm get-parameters --name ausgin.catalog.db-password --region ap-southeast-2 --with-decryption --query Parameters[0].Value )
-DB_PASSWORD=`echo $DB_PASSWORD | sed -e 's/^"//' -e 's/"$//'`
+#DB_PASSWORD=`echo $DB_PASSWORD | sed -e 's/^"//' -e 's/"$//'`
 sed -i 's/__PASSWORD__/'"${DB_PASSWORD}"'/' src/main/webapp/WEB-INF/config-db/jdbc.properties
 
 
