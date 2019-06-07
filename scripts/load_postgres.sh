6a614ed8156786db4b0900b1159661f5dd47c912
michaelsexton <michael.sexton@ga.gov.au>
2019-06-07 16:58:39 +1000

    Fix password variable

--- scripts/load_postgres.sh
+++ scripts/load_postgres.sh
@@ -5,7 +5,7 @@ pushd ~ubuntu/geonetwork
 
 DB_PASSWORD=$( aws ssm get-parameters --name ausgin.catalog.db-password --region ap-southeast-2 --with-decryption --query Parameters[0].Value )
 
-DB_PASSWORD=`echo $password | sed -e 's/^"//' -e 's/"$//'`
+DB_PASSWORD=`echo $DB_PASSWORD | sed -e 's/^"//' -e 's/"$//'`
 
 sed -i 's/__PASSWORD__/'"${DB_PASSWORD}"'/' src/main/webapp/WEB-INF/config-db/jdbc.properties
 
