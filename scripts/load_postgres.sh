7778a5661637cfb3b915961af89fbe45d227b25e
Michael Sexton <michael.sexton@ga.gov.au>
2019-06-11 13:42:47 +1000

    Change parameter name

--- scripts/load_postgres.sh
+++ scripts/load_postgres.sh
@@ -5,7 +5,7 @@ pushd ~ubuntu/geonetwork
 chown -R ubuntu *
 
 # Get RDS password from key store and rewrite JDBC config
-DB_PASSWORD=$( aws ssm get-parameters --name ausgin.catalog.db-password --region ap-southeast-2 --with-decryption --query Parameters[0].Value )
+DB_PASSWORD=$( aws ssm get-parameters --name ausgin.catalog.db.password --region ap-southeast-2 --with-decryption --query Parameters[0].Value )
 DB_PASSWORD=`echo $DB_PASSWORD | sed -e 's/^"//' -e 's/"$//'`
 sed -i 's/__PASSWORD__/'"${DB_PASSWORD}"'/' src/main/webapp/WEB-INF/config-db/jdbc.properties
 
